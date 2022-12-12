cb93a5b431b161f3f3801b9c47b2347075c5be95
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-12-12 14:11:34 -0800

    add style and code for color-button

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [buttonRed, setButtonRed] = useState(true);
-
+  //const [buttonRed, setButtonRed] = useState(true);
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div className="App">
       <button 
-      style={ buttonRed ? { backgroundColor: 'red' } : { backgroundColor: 'blue' }} 
-      onClick={()=>setButtonRed(!buttonRed)}
+        onClick={()=>setButtonColor(newButtonColor)}
+        style={{
+          backgroundColor: buttonColor, 
+          color: 'white', 
+          padding: '10px 20px', 
+          border: 'none', 
+          borderRadius: '5px', 
+          fontSize: '1.2rem'
+        }} 
       >
-        {buttonRed ? 'Change to blue' : 'Change to red'}
+        Change to {newButtonColor}
       </button>
     </div>
   );
